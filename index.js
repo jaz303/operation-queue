277b2261c691f09c5548fa883a6d03835d1b5ba0
Jason Frame <jason@onehackoranother.com>
2015-08-16 23:53:41 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 module.exports = OperationQueue;
 
-var S_READY		= 1,
-	S_BUSY		= 2,
-	S_CLOSING	= 3,
-	S_CLOSED	= 4;
+var S_READY     = 1,
+    S_BUSY      = 2,
+    S_CLOSING   = 3,
+    S_CLOSED    = 4;
 
 var CANCELLED = OperationQueue.CANCELLED = {};
 
 function OperationQueue() {
-	this._jobs = [];
-	this._state = S_READY;
-	this._onClose = null;
+    this._jobs = [];
+    this._state = S_READY;
+    this._onClose = null;
 }
 
 OperationQueue.prototype.push = function(fn, cb) {
-	if (this._state > S_BUSY) {
-		setTimeout(function() { cb(new Error(CANCELLED)); }, 0);
-	} else {
-		this._jobs.push(fn, cb);
-		if (this._state === S_READY) {
-			this._drain();
-		}
-	}
+    if (this._state > S_BUSY) {
+        setTimeout(function() { cb(new Error(CANCELLED)); }, 0);
+    } else {
+        this._jobs.push(fn, cb);
+        if (this._state === S_READY) {
+            this._drain();
+        }
+    }
 }
 
 OperationQueue.prototype.close = function(cb) {
-	if (this._state === S_READY || this._state === S_CLOSED) {
-		this._state = S_CLOSED;
-		cb && setTimeout(cb, 0);
-		return;
-	} else if (this._state === S_BUSY) {
-		this._state = S_CLOSING;
-		this._onClose = [];
-	}
-	cb && this._onClose.push(cb);
+    if (this._state === S_READY || this._state === S_CLOSED) {
+        this._state = S_CLOSED;
+        cb && setTimeout(cb, 0);
+        return;
+    } else if (this._state === S_BUSY) {
+        this._state = S_CLOSING;
+        this._onClose = [];
+    }
+    cb && this._onClose.push(cb);
 }
 
 OperationQueue.prototype._drain = function() {
-	var self = this;
-	this._state = S_BUSY;
-	(function _next() {
-		var op = self._jobs.shift(), cb = self._jobs.shift();
-		op(function() {
-			cb && cb.apply(null, arguments);
-			if (self._state === S_CLOSING) {
-				while (self._jobs.length) {
-					var _ = self._jobs.shift(), cancel = self._jobs.shift();
-					cancel && cancel(CANCELLED);
-				}
-				self._state = S_CLOSED;
-				self._onClose.forEach(function(fn) { fn(); });
-			} else if (self._jobs.length) {
-				_next();
-			} else {
-				self._state = S_READY;
-			}
-		});
-	})();
+    var self = this;
+    this._state = S_BUSY;
+    (function _next() {
+        var op = self._jobs.shift(), cb = self._jobs.shift();
+        op(function() {
+            cb && cb.apply(null, arguments);
+            if (self._state === S_CLOSING) {
+                while (self._jobs.length) {
+                    var _ = self._jobs.shift(), cancel = self._jobs.shift();
+                    cancel && cancel(CANCELLED);
+                }
+                self._state = S_CLOSED;
+                self._onClose.forEach(function(fn) { fn(); });
+            } else if (self._jobs.length) {
+                _next();
+            } else {
+                self._state = S_READY;
+            }
+        });
+    })();
 }
--- test/all.js
+++ test/all.js
@@ -3,82 +3,82 @@ var test = require('tape');
 
 test("serial execution", function(assert) {
 
-	assert.plan(1);
+    assert.plan(1);
 
-	var q = new OQ();
-	var out = '';
+    var q = new OQ();
+    var out = '';
 
-	q.push(function(done) { setTimeout(function() { out += 'a'; done(); }, 0); });
-	q.push(function(done) { setTimeout(function() { out += 'b'; done(); }, 0); });
-	q.push(function(done) { setTimeout(function() { out += 'c'; done(); }, 0); });
-	q.push(function(done) {
-		setTimeout(function() {
-			assert.equal(out, 'abc');
-			done();
-		}, 0);
-	});
+    q.push(function(done) { setTimeout(function() { out += 'a'; done(); }, 0); });
+    q.push(function(done) { setTimeout(function() { out += 'b'; done(); }, 0); });
+    q.push(function(done) { setTimeout(function() { out += 'c'; done(); }, 0); });
+    q.push(function(done) {
+        setTimeout(function() {
+            assert.equal(out, 'abc');
+            done();
+        }, 0);
+    });
 
 });
 
 test("callback fires", function(assert) {
 
-	assert.plan(3);
+    assert.plan(3);
 
-	var q = new OQ();
-	var x = 0;
+    var q = new OQ();
+    var x = 0;
 
-	q.push(
-		function(done) {
-			setTimeout(function() {
-				x = 1;
-				done(null, 10);
-			}, 0);
-		},
-		function(err, res) {
-			assert.equal(err, null);
-			assert.equal(res, 10);
-			assert.equal(x, 1);
-		}
-	);
+    q.push(
+        function(done) {
+            setTimeout(function() {
+                x = 1;
+                done(null, 10);
+            }, 0);
+        },
+        function(err, res) {
+            assert.equal(err, null);
+            assert.equal(res, 10);
+            assert.equal(x, 1);
+        }
+    );
 
 });
 
 test("cancellation cancels remaining operations", function(assert) {
 
-	assert.plan(5);
+    assert.plan(5);
 
-	var q = new OQ();
-	var out = '';
+    var q = new OQ();
+    var out = '';
 
-	q.push(function(done) {
-		setTimeout(function() {
-			out += 'a';
-			done();
-		}, 200);
-	});
+    q.push(function(done) {
+        setTimeout(function() {
+            out += 'a';
+            done();
+        }, 200);
+    });
 
-	q.push(function(done) {
-		setTimeout(function() {
-			out += 'b';
-			done();
-		}, 0);
-	}, function(err, val) {
-		assert.ok(err);
-		assert.equal(err, OQ.CANCELLED);
-	});
+    q.push(function(done) {
+        setTimeout(function() {
+            out += 'b';
+            done();
+        }, 0);
+    }, function(err, val) {
+        assert.ok(err);
+        assert.equal(err, OQ.CANCELLED);
+    });
 
-	q.push(function(done) {
-		setTimeout(function() {
-			out += 'c';
-			done();
-		}, 0);
-	}, function(err, val) {
-		assert.ok(err);
-		assert.equal(err, OQ.CANCELLED);
-	});
+    q.push(function(done) {
+        setTimeout(function() {
+            out += 'c';
+            done();
+        }, 0);
+    }, function(err, val) {
+        assert.ok(err);
+        assert.equal(err, OQ.CANCELLED);
+    });
 
-	q.close(function() {
-		assert.equal(out, 'a');
-	});
+    q.close(function() {
+        assert.equal(out, 'a');
+    });
 
 });
